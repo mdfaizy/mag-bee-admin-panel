de320d515a13b5b7a982512633632233c5eb09a3
mdfaizy <mdgmfaizy@gmail.com>
2026-03-09 16:49:40 +0530

    adding validation

--- src/services/lib/socket.ts
+++ src/services/lib/socket.ts
@@ -1,3 +1,19 @@
+// "use client";
+
+// import { io, Socket } from "socket.io-client";
+
+// let socket: Socket | null = null;
+
+// export const getSocket = (): Socket => {
+//   if (!socket) {
+//     socket = io(process.env.NEXT_PUBLIC_SOCKET_URL!, {
+//       withCredentials: true,
+//       transports: ["websocket"], // 🔥 best practice
+//     });
+//   }
+//   return socket;
+// };
+
 "use client";
 
 import { io, Socket } from "socket.io-client";
@@ -8,7 +24,7 @@ export const getSocket = (): Socket => {
   if (!socket) {
     socket = io(process.env.NEXT_PUBLIC_SOCKET_URL!, {
       withCredentials: true,
-      transports: ["websocket"], // 🔥 best practice
+      transports: ["websocket", "polling"], // ✅ correct
     });
   }
   return socket;
